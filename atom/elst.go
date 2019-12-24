330fc0b121d63cdad43249b84524a7fb2db639a9
sanil <sr1990003@gmail.com>
2019-12-24 12:04:36 -0800

    Changing field name in ElstBox.

--- atom/elst.go
+++ atom/elst.go
@@ -8,7 +8,7 @@ type ElstBox struct {
 	*Box
 	Version    uint32
 	EntryCount uint32
-	mEntries   []ElstEntry
+	Entries    []ElstEntry
 }
 
 type ElstEntry struct {
@@ -22,15 +22,14 @@ func (b *ElstBox) parse() error {
 	data := b.ReadBoxData()
 	b.Version = binary.BigEndian.Uint32(data[0:4])
 	b.EntryCount = binary.BigEndian.Uint32(data[4:8])
-	b.mEntries = make([]ElstEntry, b.EntryCount)
+	b.Entries = make([]ElstEntry, b.EntryCount)
 
-	for i := 0; i < len(b.mEntries); i++ {
-		b.mEntries[i].SegmentDuration = binary.BigEndian.Uint32(data[(8 + 12*i):(12 + 12*i)])
-		b.mEntries[i].MediaTime = binary.BigEndian.Uint32(data[(12 + 12*i):(16 + 12*i)])
-		b.mEntries[i].Rate = binary.BigEndian.Uint16(data[(16 + 12*i):(18 + 12*i)])
-		b.mEntries[i].MediaRateFraction = binary.BigEndian.Uint16(data[(18 + 12*i):(20 + 12*i)])
+	for i := 0; i < len(b.Entries); i++ {
+		b.Entries[i].SegmentDuration = binary.BigEndian.Uint32(data[(8 + 12*i):(12 + 12*i)])
+		b.Entries[i].MediaTime = binary.BigEndian.Uint32(data[(12 + 12*i):(16 + 12*i)])
+		b.Entries[i].Rate = binary.BigEndian.Uint16(data[(16 + 12*i):(18 + 12*i)])
+		b.Entries[i].MediaRateFraction = binary.BigEndian.Uint16(data[(18 + 12*i):(20 + 12*i)])
 	}
 
 	return nil
 }
-
