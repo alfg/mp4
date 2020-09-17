affcb83310727ab2828be28fc9192430e03eb44f
Alf <alf.g.jr@gmail.com>
2020-09-16 19:54:01 -0700

    Fix server example to support Go <1.9.

--- examples/server_example/main.go
+++ examples/server_example/main.go
@@ -14,20 +14,29 @@ type response struct {
 	Duration         uint32   `json:"duration"`
 }
 
+// For Go <1.9.
+type sizer interface {
+	Size() int64
+}
+
 func upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		w.Write([]byte(`invalid method`))
 	}
 	r.ParseMultipartForm(10 << 20)
 
-	file, handler, err := r.FormFile("file")
+	file, _, err := r.FormFile("file")
 	if err != nil {
 		w.Write([]byte(`file required`))
 		return
 	}
 	defer file.Close()
 
-	mp4, err := mp4.OpenFromReader(file, handler.Size)
+	// In Go 1.9+, we can just use multipart.FileHeader.Size from r.FormFile.
+	sz, _ := file.(sizer)
+	size := sz.Size()
+
+	mp4, err := mp4.OpenFromReader(file, size)
 	if err != nil {
 		fmt.Println(err)
 		return
